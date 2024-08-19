824139f4d06be30f901c3a0c2d79c4c831dade35
Swordfish <swordfish@swordfish_netology.com>
2024-08-19 15:37:58 +0300

    task final commit

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,20 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let returnValue = {data : {client: '', order: {}, goods: []}};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        returnValue.data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    returnValue.data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+    returnValue.data.order.sum = sum;
 
-    data.client = 'Иван';
+    returnValue.data.client = `${name} ${phone}`;
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify(returnValue);
 
     return jsonData;
 }
